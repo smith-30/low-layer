55f0dd5913b30fb20d4e9501e970dc55a7bb394d
misu <parkour12019@gmail.com>
2018-07-20 21:57:41 +0900

    update: asm

---
+++ nasm-gcc-container/nasm-gcc/src/hello.asm
@@ -0,0 +1,12 @@
+global _start
+
+section .data
+message: db 'hello, world!', 10
+
+section .text
+_start:
+    mov rax, 1 ; システムコールの番号をraxに入れる
+    mov rdi, 1 ; 引数 #1 は rdi: 書き込み先(descripter)
+    mov rsi, message ; 引数 #2 は rsi: 文字列の先頭
+    mov rdx, 14 ; 引数 #3 は rdx: 書き込むバイト数
+    syscall ; システムコールの呼び出し
